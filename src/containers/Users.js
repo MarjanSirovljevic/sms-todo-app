805da0663baedf5116bed650f8b897a4bc5a6230
vvll99 <10vladan@gmail.com>
2018-04-22 23:24:23 +0200

    added users and emails in table

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 class Users extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      users: [],
+    }
   }
 
   componentDidMount(){
@@ -11,14 +14,32 @@ class Users extends React.Component {
             .then(json => this.setState({users: json}));
 }
 
-  render(){
-    return(
+render(){
+  return(
       <div>
+        {<table>
+          <tbody>
+          <tr>
+            <th>Full name</th>
+            <th>Email</th> 
+          </tr>
+         
         
+        {
+          this.state.users.map(
+              (user) =>
+                <tr key={user.id}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{<a href="/link/to/">Details</a>}</td>
+                </tr>
+          )
+        } 
+        </tbody>
+        </table>}
       </div>
-    )
-  }
-
+  )
+}
 }
 
 export default Users;
