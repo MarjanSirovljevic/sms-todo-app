b9ae67594efe5f4ae59f638b6e7ee28f375b57d7
vvll99 <10vladan@gmail.com>
2018-04-23 14:00:28 +0200

    added home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Home = () => (
   <div className="main">
-    Home Page
+    <h1>SMS ToDo App</h1>
+    <h2>Get life under control</h2>
+    <p>with the ultimate app for getting things done</p>
+    <br/>
+    <h2>What is sms todo app?</h2>
+    <p>Lorem ipsum dolor sit amet, vis cu eirmod senserit, ne iuvaret pertinax intellegebat mel.
+       Cu sea homero qualisque, ei sea esse cibo omittam.
+       Has volutpat euripidis no, omnium explicari at sea, pro quis dolores sapientem et. 
+       Noluisse placerat mediocritatem vel id. Nam id utroque honestatis.
+    </p>
+    
   </div>
 );
 
