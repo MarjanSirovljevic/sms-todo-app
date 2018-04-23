ace0d43ca3a91fa8dd222c1bda8de729b48adf07
MarjanSirovljevic <marjansirovljevic@gmail.com>
2018-04-23 13:11:45 +0200

    defining all login input fields

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import Button from '../components/Button';
+import Input from '../components/Input';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.state = {
+      isValid: true,
+      errorMessage: null
+    };
+  }
+  login() {
+    console.log('login');
+  }
   render() {
-    return (
-      <div className="main">
-        Login Component
-      </div>
-    );
+    if(this.props.redirectToReferrer) {
+      return <Redirect to="/"/>
+    }
+    else {
+      return (
+        <div className="main">
+          <h1>Login Page</h1>
+          {!this.state.isValid && <p style={{color: 'red'}}>{this.state.errorMessage}</p>}
+          <Input
+            type="text"
+            name="userOrEmail"
+            placeholder="Username or Email"
+            label="Username or Email"
+            handleChange={this.handleChange}
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            label="Password"
+            handleChange={this.handleChange}
+          />
+          <Button onClick={this.login} name="Login" />
+        </div>
+      );
+    }
   }
 }
