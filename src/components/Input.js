ebb46c73a804fff681824f1198bf6bcb76f947ea
MarjanSirovljevic <marjansirovljevic@gmail.com>
2018-04-23 10:31:41 +0200

    creating Button and Input components and adding them to the Register

---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <label>generic</label>
+        <br />
+        <input type="text" />
+      </div>
+    );
+  }
+}
--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
 export default class Register extends React.Component {
   render() {
     return (
-      <div className="main">
-        Register Component
+      <div className="main" style={{marginTop: '50px'}}>
+        <Input />
+        <Button />
       </div>
     );
   }
