4a99659f185224a92e6e298369da9541101f01c9
vvll99 <10vladan@gmail.com>
2018-04-24 20:27:35 +0200

    added title and about app

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Home = () => (
   <div className="main">
-    <h1>SMS ToDo App</h1>
-    <h2>Get life under control</h2>
-    <p>with the ultimate app for getting things done</p>
-    <br/>
-    <h2>What is sms todo app?</h2>
-    <p>Lorem ipsum dolor sit amet, vis cu eirmod senserit, ne iuvaret pertinax intellegebat mel.
-       Cu sea homero qualisque, ei sea esse cibo omittam.
-       Has volutpat euripidis no, omnium explicari at sea, pro quis dolores sapientem et. 
-       Noluisse placerat mediocritatem vel id. Nam id utroque honestatis.
-    </p>
-    
-  </div>
+    <h1>SMS To-Do </h1>
+      <h2>Time to organize your work</h2>
+        <p>with this app for getting things done</p>
+        <br/>
+      <h2>What is SMS to-do app?</h2>
+        <p>SMS to-do app is a simple way to track tasks that need to be done.
+           This application hold tasks, which are the main app entities.
+           Application supports creating, listing, editing and deleting tasks.
+           Also, individual tasks can be easily distributed to the desired users
+           depending on the need.</p>
+        <br/>
+    </div>
 );
 
 export default Home;
