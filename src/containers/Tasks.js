e87473d6add0538035f7d9045350ed6e58ac13b2
MarjanSirovljevic <marjansirovljevic@gmail.com>
2018-04-25 09:27:23 +0200

    connecting todos Api to Tasks component

--- src/containers/Tasks.js
+++ src/containers/Tasks.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
-const Tasks = () => (
-  <div className="main">
-    Tasks Page
-  </div>
-);
-
-export default Tasks;
+export default class Tasks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+  componentWillMount() {
+    try {
+      const todos = JSON.parse(window.localStorage.getItem('todos'));
+      if (!todos || !Object.keys(todos)) {
+        return;
+      }
+      this.setState(() => ({ todos }));
+    } catch (error) {
+      // do nothing
+    }
+  }
+  componentDidMount(){
+    if(this.state.todos.length === 0) {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => {
+        const todos = json.map((todo) => {
+          return {...todo, userId: todo.userId.toString(), id: todo.id.toString()}
+        });
+        this.setState(() => ({ todos }));
+      })
+      .catch(error => console.log(error));
+    }
+  }
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="main">
+        Tasks Page TEST
+      </div>
+    );
+  }
+}
